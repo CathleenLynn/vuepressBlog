2b4810a12ac9cfc4f2f15db325a5ce4d255ca500
林语萱(10033001) <linyx-a@glodon.com>
2020-09-28 13:57:00 +0800

    second

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,15 +6,30 @@ module.exports = {
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
-      lineNumbers: false // 代码块显示行号
+      lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
       nav:[ // 导航栏配置
-        {text: '前端基础', link: '/accumulate/' },
-        {text: '算法题库', link: '/algorithm/'},
-        {text: '微博', link: 'https://baidu.com'}      
+        {text: '前端基础', link: 'https://baidu.com' },
+        {text: '算法题库', link: 'https://baidu.com'},
+        {text: '微博', link: 'https://baidu.com'},
+        // 多项，下拉形式
+        {
+          text: '自定义1',
+          items: [
+            {text: 'aaa', link: 'https://baidu.com'},
+          ]
+        },
+        {
+          text: '自定义2',
+          items: [
+            {text: '111', link: 'https://baidu.com'},
+            {text: '222', link: 'https://baidu.com'},
+          ]
+        },      
       ],
       sidebar: 'auto', // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
+      lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
     }
   };
