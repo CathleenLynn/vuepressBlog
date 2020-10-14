44013f4fd0b6ca3a8bb5837f04af7a473c3b3528
林语萱(10033001) <linyx-a@glodon.com>
2020-10-14 16:25:13 +0800

    bgm

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -85,6 +85,12 @@ module.exports = {
         // "email": "1156743527@qq.com",
         "link": "https://zhaoshier.top/"
       },
+      // {
+      //   "title": "赵十二",
+      //   "desc": "Enjoy when you can, and endure when you must.",
+      //   // "email": "1156743527@qq.com",
+      //   "link": "https://zhaoshier.top/"
+      // },
       {
         "title": "王金山",
         "desc": "Good Good Study，Day Day Up！！！",
@@ -109,6 +115,100 @@ module.exports = {
   },
   "markdown": {
     "lineNumbers": true
-  }
+  },
+  plugins: [
+    [
+      //先安装在配置， npm install @vuepress-reco/vuepress-plugin-kan-ban-niang --save
+      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+      {
+        theme: ['blackCat', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+        clean: false,
+        messages: {
+          welcome: '我是lookroot欢迎你的关注 ',
+          home: '心里的花，我想要带你回家。',
+          theme: '好吧，希望你能喜欢我的其他小伙伴。',
+          close: '再见哦'
+        },
+        width: 240,
+        height: 352
+      }
+    ],
+    [
+      // 彩带背景 先安装在配置， npm install vuepress-plugin-ribbon --save
+      "ribbon",
+      {
+        size: 90,     // width of the ribbon, default: 90
+        opacity: 0.8, // opacity of the ribbon, default: 0.3
+        zIndex: -1    // z-index property of the background, default: -1
+      }
+    ],
+    [
+      //鼠标点击特效 先安装在配置， npm install vuepress-plugin-cursor-effects --save
+      "cursor-effects",
+      {
+        size: 3,                    // size of the particle, default: 2
+        shape: ['circle'],  // shape of the particle, default: 'star'
+        zIndex: 999999999           // z-index property of the canvas, default: 999999999
+      }
+    ],
+    [
+      //动态标题 先安装在配置， npm install vuepress-plugin-dynamic-title --save
+      "dynamic-title",
+      {
+        showIcon: "/favicon.ico",
+        showText: "(/≧▽≦/)咦！又好了！",
+        hideIcon: "/failure.ico",
+        hideText: "(●—●)喔哟，崩溃啦！",
+        recoverTime: 2000
+      }
+    ],
+    ['meting', {
+      // metingApi: "https://meting.sigure.xyz/api/music",
+      meting: {
+        // 网易
+        server: "netease",
+        // 读取歌单
+        type: "playlist",
+        mid: "13529775",
+      },          
+      // 不配置该项的话不会出现全局播放器
+      aplayer: {
+        // 吸底模式
+        fixed: true,
+        mini: true,
+        // 自动播放
+        autoplay: true,
+        // 歌曲栏折叠
+        listFolded:true,
+        // 颜色
+        theme: '#f9bcdd',
+        // 播放顺序为随机
+        order: 'random',
+        // 初始音量
+        volume: 0.1,
+        // 关闭歌词显示
+        lrcType: 0
+      },
+      mobile :{
+        // 手机端去掉cover图
+        cover: false,
+      }
+    }]
+    // [
+    //   //先安装在配置， npm install @vuepress-plugin-meting --save
+    //   'meting', {
+    //     metingApi: "https://api.i-meto.com/meting/api",
+    //     meting: {
+    //       server: "netease",
+    //       type: "playlist",
+    //       mid: "621465725"
+    //     },          // 不配置该项的话不会出现全局播放器
+    //     aplayer: {
+    //       lrcType: 3
+    //     }
+    //   }
+    // ]
+  ]
 }
+
 
